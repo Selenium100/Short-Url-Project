561799b376fb36115f99380c38b4af70d5953a94
Nitya <nityaranjan.behera@sapiens.com>
2023-10-02 11:24:06 +0530

    test: added files on views and middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,31 +1,30 @@
+const { getUser } = require("../service/auth");
 
-const {getUser} = require('../service/auth')
+async function restrictToLoginForUserOnly(req, res, next) {
+  const userUUID = req.cookies?.token;
+  if (!userUUID) {
+    return res.redirect("/login");
+  }
 
-async function restrictToLoginForUserOnly(req,res,next){
-   const userUUID = req.cookies?.token;
-   if(!userUUID){
-    return res.redirect('/login')
-   }
+  const user = getUser(userUUID);
+  if (!user) {
+    return res.redirect("/login");
+  }
 
-   const user = getUser(userUUID);
-   if(!user){
-    return res.redirect('/login')
-   }
-
-   req.user = user;
-   next();
+  req.user = user;
+  next();
 }
 
-async function checkAuth(req,res,next){
-    const userUUID = req.cookies?.token;
- 
-    const user = getUser(userUUID);
- 
-    req.user = user;
-    next();
+async function checkAuth(req, res, next) {
+  const userUUID = req.cookies?.token;
+
+  const user = getUser(userUUID);
+
+  req.user = user;
+  next();
 }
 
 module.exports = {
-    restrictToLoginForUserOnly,
-    checkAuth
-}
+  restrictToLoginForUserOnly,
+  checkAuth,
+};
